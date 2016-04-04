2dc6966bc42d38df7645d5d0d2fe3d9265992fc6
Johan Willard <Johan Willard>
2016-04-04 15:25:22 +0200

    nuså

---
+++ Labbar/EDAF05/src/lab1/Gsalg.java
@@ -0,0 +1,168 @@
+package lab1;
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.Scanner;
+
+public class Gsalg {
+
+	/*
+	1. Read
+	2. Sort men and women accord. to name
+	3. Insert into linked list
+	4. Make preference lists
+	5. Start algorithm
+	*/
+
+	private int n;
+	private String[] people; //name and number of a man
+	private LinkedList<Integer> singleMen; //list of single men
+	private ArrayList<Integer>[] manPref; //array of the men's preferences in stacks
+	private int[][] womanPref; //2d array of women pref
+	private int[] engaged; //returns the man the woman i is engaged to, otherwise null
+	private File inFile;
+
+	public static void main(String[] args) {
+		new Gsalg().run(args);
+	}
+
+	public void run(String[] args){
+		if (args.length > 0) {
+			inFile = new File(args[0]);
+		}
+		readFile();
+
+		for(int i=1; i<= 2*n; i++) {
+			if(i%2 == 1) {
+				singleMen.add(i);
+			} else {
+				engaged[i] = -1;
+			}
+		}
+
+		galeshapley();
+		writeFile();
+	}
+
+	private void readFile() {
+		Scanner sc = null;
+		int numPer;
+		LinkedList<Integer> mrank = new LinkedList<Integer>();
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		try {sc = new Scanner(inFile);}
+		catch (IOException e) { e.printStackTrace();}
+		try {
+			String str;
+
+			while (sc.hasNext()) {
+				str = sc.nextLine();
+				while(str.startsWith("#")){
+					str = sc.nextLine();
+				}
+				if(str.startsWith("n")){
+					n = Integer.parseInt(str.substring(2));
+					people = new String[2*n];
+					singleMen = new LinkedList<Integer>();
+					manPref = (ArrayList<Integer>[]) new ArrayList[2*n];
+					for(int i=0; i<2*n; i++) {manPref[i] = new ArrayList<Integer>();}
+					womanPref = new int[2*n][2*n];
+					engaged = new int[2*n];
+					for(int i=1; i<= 2*n; i++){
+						numPer = sc.nextInt();
+						String namePer = sc.next();
+						people[numPer] = namePer;
+					}
+				} else if(str.length() != 0) {
+					String[] split = str.split(":");
+					numPer = Integer.parseInt(split[0]);
+					Scanner nSC = new Scanner(split[1]);
+					for(int i=1; i <= n ; i++) {
+						int rank = nSC.nextInt();
+						if(numPer%2 == 1){
+							manPref[numPer].add(rank);
+						} else {
+							womanPref[numPer][rank] = i;
+						}
+					}
+					/*if(numPer%2 == 1){
+						manPref.put(numPer, mrank);
+						System.out.println("Man"+numPer+".");
+						System.out.println(mrank.remove(0));
+						System.out.println(mrank.remove(0));
+						System.out.println(mrank.remove(0));
+						mrank.clear();
+					}*/
+
+				}		
+
+			}
+
+
+
+		
+		} finally {
+			sc.close();
+		}
+	}
+
+	private void writeFile() {
+
+		for (int i=2; i<=2*n; i++) {
+			if(i%2 == 0){
+				System.out.println(i + "  " + engaged[i]);
+			}
+
+		}
+
+		PrintWriter writer = null;
+		try {writer = new PrintWriter(inFile+"out.txt");}
+ 		catch (IOException e) {	e.printStackTrace();}
+
+		for (int i=2; i <= 2*n; i ++) {
+			if (i%2 == 0)
+				writer.println(people[i] + " -- " + people[engaged[i]]);
+		}
+		writer.close();
+	}
+	private void galeshapley(){
+
+		System.out.println("Women pref");
+		System.out.println(womanPref[6][1]);
+		System.out.println(womanPref[6][3]);
+		System.out.println(womanPref[6][5]);
+
+		while(singleMen.size()!=0){
+			/*System.out.println(singleMen.peek());
+			char test = (char)singleMen.peek();
+			int man = Character.getNumericValue(test);*/
+			int man = singleMen.getFirst();
+
+
+			System.out.println("Man = "+man);
+			//LinkedList<Integer> mrank = (LinkedList<Integer>)manPref.get(man);
+			//System.out.println(mrank.size());
+			int woman = manPref[man].remove(0);
+			//manPref.put(man, mrank);
+			System.out.println("Woman = "+woman);
+
+			if (engaged[woman] < 0) {
+				engaged[woman] = man;
+				singleMen.remove(0);
+				System.out.println("engaged = yes");
+			} else if (engaged[woman] > 0 && (womanPref[woman][engaged[woman]] > womanPref[woman][man])) {
+				singleMen.remove(0);
+				singleMen.add(engaged[woman]);
+				engaged[woman] = man;
+				System.out.println("engaged = yes");
+			} else {
+				System.out.println("engaged = no");
+				System.out.println(engaged[woman]);
+			}
+		}
+	}
+
+}
