74557bd758e7f25678ee7a004ed9bc90e146b83c
RobertErlandsson <dat12rer@student.lu.se>
2016-04-11 13:18:42 +0200

    labb3 edge & spanning

--- Labbar/EDAF05/src/Labb3/SpanningUSA.java
+++ Labbar/EDAF05/src/Labb3/SpanningUSA.java
@@ -1,5 +1,47 @@
 package Labb3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class SpanningUSA {
+	static HashMap<String, LinkedList<Edge>> list = new HashMap<String, LinkedList<Edge>>();
+
+	
+	public static void readfile(String filename) {
+		File f = new File(filename);
+		Scanner scan = null;
+		try {
+			scan = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		while (scan.hasNext()) {
+			String line = scan.nextLine();
+			if (!line.contains("--")) {
+				list.put(line, new LinkedList<Edge>());
+			}
+			if (line.contains("--")) {
+				String[] splitCities = line.split("--");
+				splitCities[0] = splitCities[0].trim();
+
+			} 
+		}
+		scan.close();
+	}
+	public static void main(String[] args) {
+
+		readfile("src/Labb3/USA-highway-miles.txt");
+	}
+	
 
 }
