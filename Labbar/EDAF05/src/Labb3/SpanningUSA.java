b02bc48b90244cd23196b549989a4b2f4259bbc6
RobertErlandsson <dat12rer@student.lu.se>
2016-04-11 15:32:07 +0200

    spanning

--- Labbar/EDAF05/src/Labb3/SpanningUSA.java
+++ Labbar/EDAF05/src/Labb3/SpanningUSA.java
@@ -22,12 +22,42 @@ public class SpanningUSA {
 		while (scan.hasNext()) {
 			String line = scan.nextLine();
 			if (!line.contains("--")) {
-				list.put(line.trim(), new LinkedList<Edge>());
+				line = line.trim();
+				if(line.indexOf("\"") != -1){
+					list.put(line.substring(line.indexOf("\"")+1, line.lastIndexOf("\"")),new LinkedList<Edge>());
+				
+				} else {
+					list.put(line, new LinkedList<Edge>());
+				}
 			}
 			if (line.contains("--")) {
+				String source;
+				String target;
+				int weight;
 				String[] splitCities = line.split("--");
 				splitCities[0] = splitCities[0].trim();
+				
+				if(splitCities[0].indexOf("\"") != -1){
+					source = splitCities[0].substring(splitCities[0].indexOf("\"")+1, splitCities[0].lastIndexOf("\""));
+					System.out.println(source);
+				} else {
+					source = splitCities[0];
+				}
+				
+				String[] splitDist = splitCities[1].split("\\[");
+				splitDist[0] = splitDist[0].trim();				
 
+				if(splitDist[0].indexOf("\"") != -1){
+					target = splitDist[0].substring(splitDist[0].indexOf("\"")+1, splitDist[0].lastIndexOf("\""));
+					
+				} else {
+					target = splitDist[0];
+				}
+				
+				weight = Integer.parseInt(splitDist[1].substring(0, splitDist[1].length()-1));
+				
+				 list.get(source).add(new Edge(source, target, weight));
+			
 			} 
 		}
 		scan.close();
