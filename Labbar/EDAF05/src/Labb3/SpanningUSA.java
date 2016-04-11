d9edf83d204adc600c925a84059a2ffca8c2c3f9
Johan Willard <Johan Willard>
2016-04-11 15:31:48 +0200

    börjat med prim

--- Labbar/EDAF05/src/Labb3/SpanningUSA.java
+++ Labbar/EDAF05/src/Labb3/SpanningUSA.java
@@ -7,8 +7,14 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class SpanningUSA {
-	static HashMap<String, LinkedList<Edge>> list = new HashMap<String, LinkedList<Edge>>();
-
+	private static HashMap<String, LinkedList<Edge>> list = new HashMap<String, LinkedList<Edge>>();
+	private String[] id;
+	private int[] mstSet;
+	private String[] remainSet; 
+	private LinkedList<Edge> resSet;
+	private int distance;
+	private int size;
+	private String beginStr;
 	
 	public static void readfile(String filename) {
 		File f = new File(filename);
@@ -35,6 +41,43 @@ public class SpanningUSA {
 	public static void main(String[] args) {
 
 		readfile("src/Labb3/USA-highway-miles.txt");
+	}
+	
+	public void prim(){
+		mstSet = new int[size];
+		for(int i : mstSet) mstSet[i]=-1;
+		mstSet[0]=0;
+		id[0]=beginStr;
+		int n=0;
+		Edge eTemp = new Edge(null, null, Integer.MAX_VALUE);
+		
+		while(n < (size-1)){
+			n=0;
+			while(mstSet[n] == 0){
+				LinkedList<Edge> tempList = list.get(id[n]);
+				for(Edge e : tempList){
+					if(newConnect(e) && eTemp.compareTo(e) > 0){
+						eTemp = e;
+					}
+				}
+				n++;
+			}
+			mstSet[n] = 0;
+			id[n] = eTemp.target;
+			resSet.add(eTemp);
+			distance+=eTemp.weight;
+		}
+	}
+	
+	private boolean newConnect(Edge e) {
+		String testTarget = e.target;
+		int k = 0;
+		while(mstSet[k] == 0){
+			if(id[k].equals(testTarget)){
+				return false;
+			}
+		}
+		return true;
 	}
 	
 
